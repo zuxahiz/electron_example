8a4474b3fefc72a3bd5b07f664f9d81f1e191f01
zuxahiz <zhuxiangxian123@163.com>
2023-08-06 19:27:15 +0800

    init versioin

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // Required if you want to use Node.js APIs in your UI
+    },
+  });
+
+  mainWindow.loadFile('index.html'); // Load the HTML file
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
